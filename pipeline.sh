1056930b40d10ec8787c9d8459bd539af5a541b0
Igor null <m1el.2027@gmail.com>
2018-08-28 12:21:23 +0300

    replaced changing directories with specifying --manifest-path

--- pipeline.sh
+++ pipeline.sh
@@ -4,9 +4,7 @@ set -e
 echo "you shouldn't run this file, it's not meant to be ran" && exit 1
 youtube-dl.exe https://www.twitch.tv/videos/302423092 -o full_stream.mp4
 ffmpeg -i full_stream.mp4 -vf fps=1/10 shots/%04d.png
-cd dec
-cargo build --release
-cd ..
+cargo build --release --manifest-path dec/Cargo.toml
 dec/target/release/dec > cyberpunk2077-raw.b64
 # you need to compile the C# thingy
 ConsoleApplication69/ConsoleApplication69/bin/Debug/ConsoleApplication69.exe
